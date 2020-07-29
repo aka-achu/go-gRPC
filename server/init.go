8ef382162965b1636a9f55d71987e9836c1f4a55
Achyuta Das <aka.achu.1612@gmail.com>
2020-07-29 15:12:11 +0530

    Chore: Implementing the Unimplemented server interface

--- server/init.go
+++ server/init.go
@@ -7,14 +7,16 @@ import (
 	"net"
 )
 
-type UnimplementedOperationService struct{}
+type Service struct{
+	operation_pb.UnimplementedOperationServiceServer
+}
 
 func Initialize() {
 	if listener, err := net.Listen("tcp", "0.0.0.0:50051"); err != nil {
 		log.Fatalf("Failed to listen @'0.0.0.0:50051'-  %v", err)
 	} else {
 		s := grpc.NewServer()
-		operation_pb.RegisterOperationServiceServer(s, &UnimplementedOperationService{})
+		operation_pb.RegisterOperationServiceServer(s, &Service{})
 		if err := s.Serve(listener); err != nil {
 			log.Fatalf("failed start the serve-r %v", err)
 		}
