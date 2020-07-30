c300cef84c734833d08aaee493a1282563f0ae56
Achyuta Das <aka.achu.1612@gmail.com>
2020-07-30 08:39:30 +0530

    Add: server and client will sslmode enabled implemented

--- client/init.go
+++ client/init.go
@@ -3,13 +3,31 @@ package client
 import (
 	"github.com/aka-achu/go-gRPC/models/operation_pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 )
 
 func Initialize() {
-	if clientConnection, err := grpc.Dial(os.Getenv("SERVER_ADDRESS"), grpc.WithInsecure()); err != nil {
+	opts := grpc.WithInsecure()
+	if os.Getenv("SSL_MODE") == "true" {
+		if credential, err := credentials.NewClientTLSFromFile(
+			filepath.Join(
+				os.Getenv("CERTIFICATE_DIR"),
+				os.Getenv("CA_FILE"),
+			),
+			"localhost",
+		); err != nil {
+			log.Fatalf("Failed to load CA trust certificate. -%v", err)
+			return
+		} else {
+			opts = grpc.WithTransportCredentials(credential)
+		}
+
+	}
+	if clientConnection, err := grpc.Dial(os.Getenv("SERVER_ADDRESS"), opts); err != nil {
 		log.Fatalf("could not connect: %v", err)
 	} else {
 		defer clientConnection.Close()
--- server/init.go
+++ server/init.go
@@ -3,20 +3,39 @@ package server
 import (
 	"github.com/aka-achu/go-gRPC/models/operation_pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 	"os"
+	"path/filepath"
 )
 
-type Service struct{
+type Service struct {
 	operation_pb.UnimplementedOperationServiceServer
 }
 
 func Initialize() {
+
 	if listener, err := net.Listen("tcp", os.Getenv("SERVER_ADDRESS")); err != nil {
-		log.Fatalf("Failed to listen @'0.0.0.0:50051'-  %v", err)
+		log.Fatalf("Failed to listen @'%s'-  %v", os.Getenv("SERVER_ADDRESS"), err)
 	} else {
-		s := grpc.NewServer()
+		var opts []grpc.ServerOption
+		if os.Getenv("SSL_MODE") == "true" {
+			if credential, err := credentials.NewServerTLSFromFile(
+				filepath.Join(
+					os.Getenv("CERTIFICATE_DIR"),
+					os.Getenv("CERTIFICATE_FILE"),
+				), filepath.Join(
+					os.Getenv("CERTIFICATE_DIR"),
+					os.Getenv("KEY_FILE"),
+				),
+			); err != nil {
+				log.Fatalf("Failed to load ssl certificates. -%v", err)
+			} else {
+				opts = append(opts, grpc.Creds(credential))
+			}
+		}
+		s := grpc.NewServer(opts...)
 		operation_pb.RegisterOperationServiceServer(s, &Service{})
 		if err := s.Serve(listener); err != nil {
 			log.Fatalf("failed start the serve-r %v", err)
